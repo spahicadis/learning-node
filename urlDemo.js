8d33efe00d5758d87b85a53d1f0fb291c43252f6
spahicadis <adis@spahica.com>
2024-09-23 10:08:59 +0200

    Finished crash course, now practice these with some my own projects

--- urlDemo.js
+++ urlDemo.js
@@ -3,4 +3,22 @@ import url from 'url';
 const urlString = 'https://www.google.com/search?q=hello+world';
 
 //URL Object
+const urlObj = new URL(urlString);
+console.log(urlObj)
+
+//format()
+console.log(url.format(urlObj));
+
+//import.meta.url
+console.log(import.meta.url)
+
+console.log(url.fileURLToPath(import.meta.url));
+
+console.log(urlObj.search);
+
+const params = new URLSearchParams(urlObj.search);
+console.log(params.get('q'));
+params.append('limit', '5');
+params.delete('limit');
+console.log(params);
 
